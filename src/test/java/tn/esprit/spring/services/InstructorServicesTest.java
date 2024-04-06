9aa318a9270bdd71ff3f65f255ae98c81e9b3cf4
Youssef Chouchene <youssef.chouchene@avaxia-group.com>
2024-04-06 22:59:35 +0100

    create class InstructorServicesTest and insert instructor and course in the same time

---
+++ src/test/java/tn/esprit/spring/services/InstructorServicesTest.java
@@ -0,0 +1,50 @@
+package tn.esprit.spring.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Instructor;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootTest
+
+public class InstructorServicesTest {
+    @Autowired
+    private IInstructorServices instructorServices;
+    @Autowired
+    private ICourseServices courseServices;
+    @Test
+    public void testAddPiste() {
+        // Create add
+
+        Course course = new Course();
+        course.setLevel(1);
+        course.setTypeCourse(TypeCourse.COLLECTIVE_CHILDREN);
+        course.setSupport(Support.SNOWBOARD);
+        course.setPrice(50.0f);
+        course.setTimeSlot(60);
+        courseServices.addCourse(course);
+
+        Instructor instructor = new Instructor();
+        instructor.setNumInstructor(1L); // Set the instructor number
+        instructor.setFirstName("Youssef");
+        instructor.setLastName("chouchene");
+        instructor.setDateOfHire(LocalDate.now());
+        Set<Course> courses = new HashSet<>();
+        Course newCourse = new Course();
+        newCourse.setLevel(1);
+        courses.add(newCourse);
+        instructorServices.addInstructor(instructor);
+
+
+    }
+
+
+
+}
